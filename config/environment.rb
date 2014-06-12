06e4ec729214207639ec616ed425f244c2ba021c
Xiaodong Zhou <xdzhou@gmail.com>
2014-06-12 14:52:54 +0800

    Add a StaticPages controller

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # adding this fixes the issue
 require 'tzinfo'
+#require 'webrat/rspec-rails'
 
 # Initialize the Rails application.
 Rails.application.initialize!
